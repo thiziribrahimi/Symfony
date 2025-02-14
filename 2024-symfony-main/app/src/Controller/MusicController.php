01ebd6e4e086b97c0936f58e0318857c245470ff
thiziribrahimi <thiziribrahimi092@gmail.com>
2025-02-14 16:55:51 +0100

    affichage de musique

---
+++ 2024-symfony-main/app/src/Controller/MusicController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\MusicRepository;
+
+class MusicController extends AbstractController
+{
+    #[Route('/', name: 'app_music')]
+    public function index(
+        MusicRepository $musicRepo
+    ): Response
+    {
+        $musics = $musicRepo->findAll();
+
+        return $this->render('music/index.html.twig', [
+            'musicsList' => $musics,
+        ]);
+    }
+}
---
+++ 2024-symfony-main/app/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Music;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i=0; $i <= 10; $i++){
+            $music = new Music();
+            $music->setName("Music". $i);
+            $music->setUrl("url". $i);
+
+            $manager->persist($music);
+        }
+        $manager->flush();
+    }
+}
