ddbe2f6c93fbe109fdd1927558b60c46b63e59a1
thiziribrahimi <thiziribrahimi092@gmail.com>
2025-02-27 15:29:51 +0100

    dashboard, MusicController

--- 2024-symfony-main/app/src/DataFixtures/AppFixtures.php
+++ 2024-symfony-main/app/src/DataFixtures/AppFixtures.php
@@ -5,15 +5,21 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Music;
+use Faker\Factory;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
-    {
+    {   
+        $faker = \Faker\Factory::create();
+
+
         for ($i=0; $i <= 10; $i++){
             $music = new Music();
             $music->setName("Music". $i);
             $music->setUrl("url". $i);
+            $music->setAuthor($faker->name());
 
             $manager->persist($music);
         }
--- 2024-symfony-main/app/src/Entity/Music.php
+++ 2024-symfony-main/app/src/Entity/Music.php
@@ -28,6 +28,9 @@ class Music
     #[ORM\Column]
     private ?\DateTimeImmutable $updatedAt = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $author = null;
+
     #[ORM\PrePersist]
     public function setTimestampsValue(): void
     {
@@ -87,6 +90,18 @@ class Music
     public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?string $author): static
+    {
+        $this->author = $author;
 
         return $this;
     }
